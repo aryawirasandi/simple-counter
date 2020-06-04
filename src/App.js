0143fb25ade1c20c799ecb4f02b5f4f102a59e64
aryawirasandi <aryawirasandi7153@gmail.com>
2020-06-04 23:48:52 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import Main from './components/Main/Main.js';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Main/>
+    // <div>
+    //     <h2>Please Enter Increase / Decrease the value until you satisfied</h2>
+    //     <h1>0</h1>
+    //     <button>Add + 1</button>
+    //     <button>Minus - 1</button>
+    // </div>
   );
 }
 
