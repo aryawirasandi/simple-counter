b30281d9008509fc539236d5f1fd382ae1644fdf
aryawirasandi <aryawirasandi@github.com>
2020-07-09 02:52:42 +0700

    [REFACTOR CODE] 1. update component structure from class to SFC (Stateless Function Component) with hooks 2. update counter color code and max size

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,49 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import CounterValue from "../CounterValue/CounterValue";
 import HeadingText from "../Text/HeadingText";
 import Button from "../Buttons/Button";
 import Footer from "../Footer/Footer";
-class Main extends React.Component {
 
-    constructor() {
-        super();
-        this.state = {
-            value : 0
-        }
-        this.increaseValue = this.increaseValue.bind(this);
-        this.decreaseValue = this.decreaseValue.bind(this);
+
+
+function Main (){
+    let [number, setValue] = useState(0);
+
+    function increaseValue(){
+        number == 20 ? alert("hey you have been achieved at the hight level of this counter please decrease it") : setValue(number += 1);
     }
 
-    increaseValue(){
-        this.setState(state => (
-            {
-                value : state.value + 1
-            }
-        ))
+    function decreaseValue(){
+        number == 0 ? setValue(0) : setValue(number -= 1);
     }
 
-    decreaseValue(){
-        this.setState(state => (
-            {
-                value : state.value - 1
-            }
-        ))
-    }
-
-    render() {
-        const number = this.state.value;
-        return (
-            <div>
-                <HeadingText text="Hello Welcome to the Simple Counter, Please Hit Increase Or Decrease until you satisfied"/>
-                <CounterValue value={number}/>
-                <Button
-                    handleIncrease={this.increaseValue}
-                    handleDecrease={this.decreaseValue}
-                />
-                <Footer text="Create in one night By Arya Wirasandi"/>
-            </div>
-        );
-    }
+    return (
+        <div>
+            <HeadingText text="Hello Welcome to the Simple Counter, Please Hit Increase Or Decrease until you satisfied"/>
+            <CounterValue value={number}/>
+            <Button
+                handleIncrease={increaseValue}
+                handleDecrease={decreaseValue}
+            />
+            <Footer text="Create in one night By Arya Wirasandi"/>
+        </div>
+    );
 }
 
 export default Main;
